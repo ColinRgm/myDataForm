5e7a5f42e4f17faff1845824491a8498d6abee63
Colin <colin.regamey@jobtrek.com>
2022-02-12 11:47:11 +0100

    Edit index.php and style.css

--- index.php
+++ index.php
@@ -6,49 +6,81 @@ require_once 'DB_connection.php'; ?>
 <?php
 if (isset($_POST['submit'])) {
 
-    $stmt = $pdo->prepare(
-            'INSERT INTO Users (first_name, Last_name, birthdate, email, phone, civility, sex)
-            VALUES(:first_name, :Last_name, :birthdate, :email, :phone, :civility, :sex)'
+
+
+    /* -- Country ----------------------------------------------------------------------------- */
+    $country = $_POST['country'];
+
+    $stmt_countryIfExistOrNot = $pdo->prepare(
+            'SELECT * FROM Countries WHERE country_name = :countryName'
     );
 
-    $stmt->execute([
-         'first_name' => $_POST['first_name'],
-         'Last_name' => $_POST['Last_name'],
-         'birthdate' => $_POST['birthdate'],
-         'email' => $_POST['email'],
-         'phone' => $_POST['phone'],
-         'civility' => $_POST['civility'],
-         'sex' => $_POST['sex']
-        ]
-    );
-    $id_user = $pdo->lastInsertId();
-} var_dump($stmt);
+    $stmt_countryIfExistOrNot->execute([
+            'countryName' => $country
+    ]);
+
+    $existantCountry = $stmt_countryIfExistOrNot->fetchAll();
+
+    if ($existantCountry) {
+        $id_country = $existantCountry[0]['id_country'];
+    } else {
+        $stmt_country = $pdo->prepare(
+            'INSERT INTO Countries (country_name)
+                VALUE(:country_name)');
+        $stmt_country->execute([
+            'country_name' => $country
+        ]);
+        $id_country = $pdo->lastInsertId();
+    }
+
+
+
+        /* -- Adresses ------------------------------------------------------------------------ */
+        $stmt_adress = $pdo->prepare(
+                'INSERT INTO Adresses (street, postal_code, city, Countries_id_country)
+                VALUES (:street, :postal_code, :city, :Countries_id_country)');
+        $stmt_adress->execute([
+                'street' => $_POST['adress'],
+                'postal_code' => $_POST['npa'],
+                'city' => $_POST['city'],
+                'Countries_id_country' => $id_country,
+        ]);
+        $id_country = $pdo->lastInsertId();
+
+
+
+        /* -- Utilisateurs -------------------------------------------------------------------- */
+        $stmt = $pdo->prepare(
+                'INSERT INTO Users (first_name, Last_name, birthdate, email, phone, civility, sex)
+                VALUES(:first_name, :Last_name, :birthdate, :email, :phone, :civility, :sex)'
+        );
+
+        $stmt->execute([
+             'first_name' => $_POST['first_name'],
+             'Last_name' => $_POST['Last_name'],
+             'birthdate' => $_POST['birthdate'],
+             'email' => $_POST['email'],
+             'phone' => $_POST['phone'],
+             'civility' => $_POST['civility'],
+             'sex' => $_POST['sex']
+            ]
+        );
+        $id_user = $pdo->lastInsertId();
+
+
+
+        /* -- Adresses d'utilisateur ---------------------------------------------------------- */
+        $stmtAdressHasUsers = $pdo->prepare(
+                'INSERT INTO Adresses_has_Users (Adresses_id_adress, Users_id_user)
+                VALUES (:Adresses_id_adress, :Users_id_user)'
+        );
+        $stmtAdressHasUsers->execute([
+                'Users_id_user' => $id_user,
+                'Adresses_id_adress' => $id_address
+        ]);
+    }
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <h1>Formulaire</h1>
 <form action="index.php" method="POST">
@@ -83,16 +115,16 @@ if (isset($_POST['submit'])) {
         <input type="tel" id="phone" name="phone" aria-label="" placeholder="N° tél">
 
         <label for="adress"></label>
-        <input type="text" id="adress" name="adress" placeholder="Rue" aria-label="">
-
-        <label for="num"></label>
-        <input type="text" id="num" name="num" placeholder="N°" aria-label="">
+        <input type="text" id="adress" name="adress" placeholder="Rue et N°" aria-label="">
 
         <label for="npa"></label>
         <input type="text" id="npa" name="npa" placeholder="Code postal" aria-label="">
 
         <label for="city"></label>
         <input type="text" id="city" name="city" placeholder="Ville" aria-label="">
+
+        <label for="country"></label>
+        <input type="text" id="country" name="country" placeholder="Pays" aria-label="">
 
         <input id="button" name="submit" type="submit" value="Envoyer"/>
     </div>
